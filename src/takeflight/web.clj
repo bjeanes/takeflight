5670797e94e29af75a30d3f9a07eb10ae2dc920f
Bo Jeanes <me@bjeanes.com>
2012-08-31 16:57:43 -0500

    Start web app side to show the milestones

---
+++ src/takeflight/web.clj
@@ -0,0 +1,23 @@
+(ns takeflight.web
+  (:require [compojure.core :refer :all]
+            [takeflight.pivotal :as pt]))
+
+(def api-token "replace-me")
+
+(def webapp
+  (routes
+
+   (GET "/:id"
+        [id]
+        (if id
+
+          (str "<ol>"
+      	       (apply str (map 
+                           #(str "<li>" 
+                                 (:name %) 
+                                 " - "
+                                 (:deadline %)
+                                 "</li>") 
+                           (filter :deadline (pt/releases id api-token))))
+               "</ol>")
+          "Bad ID"))))
