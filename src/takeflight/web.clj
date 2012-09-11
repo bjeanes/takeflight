73a72fe23f7c9af9bae3ddfd1d4a5923afce9ecd
Bo Jeanes <me@bjeanes.com>
2012-09-10 23:39:55 -0500

    Make the actual web app work with new layouts

--- src/takeflight/web.clj
+++ src/takeflight/web.clj
@@ -1,70 +1,71 @@
 (ns takeflight.web
   (:require [compojure.core :refer :all]
             [compojure.route :as route]
-            [net.cgrand.enlive-html :as enlive]
+            [net.cgrand.enlive-html :as h]
             [takeflight.pivotal :as pt]
-            [takeflight.time :refer :all]
-            [ring.middleware.reload :as ring]))
+            [takeflight.time :refer :all]))
 
+;; TODO: figure out a nice way to get this right
+(def ^:private development? true)
 (def ^:private api-token (System/getenv "TOKEN"))
-
-(enlive/deftemplate not-found "public/404.html" [])
-
-(enlive/deftemplate layout "views/layout.html"
-  [title body]
-
-  [:head :title] (enlive/append (when title (str " / " title)))
-  [:body :> :h1] (enlive/after (when title (enlive/html-snippet (str "<h2>" title "</h2>"))))
-  [:div#content] (enlive/content body))
+(def ^:private template "views/layout.html")
 
 (defn- decorate-release-for-display
   [{:keys [name eta deadline project] :as release}]
 
   (let [name (str name)
         status (relative-time-for-status deadline eta)
+        status-class "on-time" ; TODO: 'late', 'early', 'etc'
         eta (day-and-month eta)
         deadline (day-and-month deadline)
         status (if (= eta deadline) "Scheduled" status)
         project-name (:name project)]
 
     {:status status
+     :status-class status-class
      :name name
      :eta eta
      :deadline deadline
      :project-name project-name}))
 
-(enlive/defsnippet flight-status-board "views/status-board.html" [:table#status-board]
+(h/deftemplate not-found "public/404.html" [])
+
+(h/deftemplate layout template
+  [body]
+
+  [:div#content] body)
+
+(h/defsnippet flight-status-board template [:table#status-board]
   [releases]
 
-  [:tbody :tr] (enlive/clone-for
-                [{:keys [name eta deadline status project-name]}
-                 (map decorate-release-for-display releases)]
+  ;; remove all rows except the first
+  [:tbody [:tr (h/but h/first-child)]] (h/substitute nil)
 
-                [:.from] (enlive/content project-name)
-                [:.to] (enlive/content name)
-                [:.arrives] (enlive/content eta)
-                [:.scheduled] (enlive/content deadline)
-                [:.status] (enlive/content status)))
+  ;; duplicate the first row for each release
+  [:tbody :tr]
+   (h/clone-for
+    [{:keys [name eta deadline status status-class project-name]}
+     (map decorate-release-for-display releases)]
 
-;; TODO: figure out a nice way to get this right
-(def ^:private development? true)
+    [:.from] (h/content project-name)
+    [:.to] (h/content name)
+    [:.arrives] (h/content eta)
+    [:.scheduled] (h/content deadline)
+    [:.status] (h/do->
+                (h/add-class status-class)
+                (h/content status))))
 
-(def ^:private handler
+
+(def webapp
   (routes
    (GET "/" []
-     (layout nil (enlive/html-content "<a href=\"/584807\">Quantum Lead</a>")))
+     (layout (h/html-content "<a href=\"/584807\">Quantum Lead</a>")))
 
    (GET ["/:id" :id #"[0-9]+"] [id]
-     (layout "Project Overview"
-             (if id
-               (flight-status-board (pt/releases+projections api-token id))
-               (enlive/content "Bad ID"))))
+     (layout (h/content (if id
+                          (flight-status-board (pt/releases+projections api-token id))
+                          "Bad ID"))))
 
    (route/resources "/")
    (when development? (route/resources "/" {:root "views"}))
    (route/not-found (not-found))))
-
-(def webapp
-  (if development?
-    (ring/wrap-reload #'handler '(takeflight.web))
-    handler))
