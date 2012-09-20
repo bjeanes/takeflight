630773bc52dc7b120f0401abf87c78b7aa63048c
Bo Jeanes <me@bjeanes.com>
2012-09-20 13:59:08 -0500

    All tickers now share a thread instead of having extra threads

--- src/takeflight/util.clj
+++ src/takeflight/util.clj
@@ -1,17 +1,26 @@
-(ns takeflight.util)
+(ns takeflight.util
+  (:import [java.util.concurrent
+            ScheduledThreadPoolExecutor
+            TimeUnit]))
+
+(def ^:private ticker-pool
+  (delay (ScheduledThreadPoolExecutor. 1)))
+
+(defn shutdown-tickers []
+  (when (realized? ticker-pool) (.shutdown @ticker-pool)))
 
 (defn tick
   "Call f with args every ms. First call will be after ms"
   [ms f & args]
 
-  (future
-    (doseq [f (repeatedly #(apply f args))]
-      (Thread/sleep ms)
-      (f))))
+  (.scheduleWithFixedDelay @ticker-pool
+                           #(apply f args)
+                           ms ms
+                           TimeUnit/MILLISECONDS))
 
 (defn tick-now
   "Call f with args every ms. First call will be immediately (and blocking)"
   [ms f & args]
 
-  (apply f args)
+  (.get (.submit @ticker-pool #(apply f args)))
   (apply tick ms f args))
